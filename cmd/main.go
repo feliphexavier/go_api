0b56df4734d590f38f46e6373d436ba6cbacd4fb
Feliphe Xavier <feliphexavier2604@gmail.com>
2025-09-29 19:09:25 -0300

    swagger added

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	_ "go_api/cmd/docs"
 	"go_api/internal/config"
 	pictureHandler "go_api/internal/handler"
 	tripHandler "go_api/internal/handler"
@@ -17,8 +18,21 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @title           Photo Album API
+// @version         1.0
+// @description     This is a sample server for a trip booking service.
+// @termsOfService  http://swagger.io/terms/
+
+//@securityDefinitions.apikey BearerAuth
+//@in header
+//@name Authorization
+//@description Type "Bearer" followed by a space and JWT token.
+
+// @host 	localhost:8080
 func main() {
 	r := gin.Default()
 	validate := validator.New()
@@ -32,6 +46,7 @@ func main() {
 	}
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+
 	r.GET("/healthy", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "ok",
@@ -53,5 +68,6 @@ func main() {
 	userHandler.RouteList(cfg.SecretJWT)
 	tripHandler.RouteList(cfg.SecretJWT)
 	server := fmt.Sprintf("127.0.0.1:%s", cfg.Port)
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	r.Run(server)
 }
